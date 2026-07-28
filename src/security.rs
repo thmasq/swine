7f5c421660981710d18b8c00474616b5952c08a2
Thomas Q <thomasqsa@gmail.com>
2026-07-28 00:30:13 -0300

    Match docker's default seccomp filter

--- src/security.rs
+++ src/security.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use caps::CapSet;
-use libseccomp::{ScmpAction, ScmpFilterContext, ScmpSyscall};
+use libseccomp::{ScmpAction, ScmpArgCompare, ScmpCompareOp, ScmpFilterContext, ScmpSyscall};
 use std::fs;
 
 pub fn lockdown(config: &crate::config::SandboxConfig) -> Result<()> {
@@ -59,6 +59,51 @@ pub fn lockdown(config: &crate::config::SandboxConfig) -> Result<()> {
             "io_uring_setup",
             "io_uring_enter",
             "io_uring_register",
+            "kcmp",
+            "fsopen",
+            "fsconfig",
+            "fsmount",
+            "fspick",
+            "move_mount",
+            "open_tree",
+            "mount_setattr",
+            "statmount",
+            "listmount",
+            "open_by_handle_at",
+            "name_to_handle_at",
+            "pidfd_getfd",
+            "process_madvise",
+            "process_mrelease",
+            "ioperm",
+            "iopl",
+            "personality",
+            "syslog",
+            "chroot",
+            "create_module",
+            "query_module",
+            "get_kernel_syms",
+            "uselib",
+            "_sysctl",
+            "sysfs",
+            "ustat",
+            "nfsservctl",
+            "quotactl",
+            "reboot",
+            "clock_adjtime",
+            "clock_settime",
+            "settimeofday",
+            "stime",
+            "get_mempolicy",
+            "set_mempolicy",
+            "mbind",
+            "move_pages",
+            "vm86",
+            "vm86old",
+            "lookup_dcookie",
+            "acct",
+            "socketcall",
+            "swapon",
+            "swapoff",
         ];
 
         for syscall_name in &blocked_syscalls {
@@ -73,8 +118,56 @@ pub fn lockdown(config: &crate::config::SandboxConfig) -> Result<()> {
             }
         }
 
+        // A. SOCKET: Block AF_ALG (38) and AF_VSOCK (40)
+        // socket(domain, type, protocol). 'domain' is argument 0.
+        if let Ok(socket_syscall) = ScmpSyscall::from_name("socket") {
+            ctx.add_rule_conditional(
+                ScmpAction::Errno(nix::libc::EPERM),
+                socket_syscall,
+                &[ScmpArgCompare::new(0, ScmpCompareOp::Equal, 38)], // AF_ALG
+            )?;
+            ctx.add_rule_conditional(
+                ScmpAction::Errno(nix::libc::EPERM),
+                socket_syscall,
+                &[ScmpArgCompare::new(0, ScmpCompareOp::Equal, 40)], // AF_VSOCK
+            )?;
+        }
+
+        // B. CLONE: Block creation of namespaces, but allow normal multithreading
+        // clone(flags, ...). 'flags' is argument 0.
+        if let Ok(clone_syscall) = ScmpSyscall::from_name("clone") {
+            let forbidden_clone_flags: [u64; 5] = [
+                0x00020000, // CLONE_NEWNS
+                0x04000000, // CLONE_NEWUTS
+                0x08000000, // CLONE_NEWIPC
+                0x20000000, // CLONE_NEWPID
+                0x40000000, // CLONE_NEWNET
+            ];
+            for flag in forbidden_clone_flags.iter() {
+                ctx.add_rule_conditional(
+                    ScmpAction::Errno(nix::libc::EPERM),
+                    clone_syscall,
+                    &[ScmpArgCompare::new(
+                        0,
+                        ScmpCompareOp::MaskedEqual(*flag),
+                        *flag,
+                    )],
+                )?;
+            }
+        }
+
+        // C. IOCTL: Explicitly block TIOCSTI (0x5412) to prevent terminal injection
+        // ioctl(fd, request, ...). 'request' is argument 1.
+        if let Ok(ioctl_syscall) = ScmpSyscall::from_name("ioctl") {
+            ctx.add_rule_conditional(
+                ScmpAction::Errno(nix::libc::EPERM),
+                ioctl_syscall,
+                &[ScmpArgCompare::new(1, ScmpCompareOp::Equal, 0x5412)], // TIOCSTI
+            )?;
+        }
+
         ctx.load()?;
-        println!("Child: Loaded Seccomp-BPF filter.");
+        println!("Child: Loaded Seccomp-BPF filter (with conditional argument blocking).");
     }
 
     Ok(())
